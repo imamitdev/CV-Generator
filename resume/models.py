d7e3b62bcfb1483166e2b89f495328d12912e10a
imamitdev <imamitsinghchauhan@gmail.com>
2024-05-31 19:47:17 +0530

    template setup

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    mobile_no = models.CharField(max_length=100)
+    summary = models.TextField(max_length=1000)
+    degree = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    university = models.CharField(max_length=100)
+    previous_work = models.TextField(max_length=1000)
+    skills = models.TextField(max_length=1000)
+    created_date = models.DateTimeField(auto_now=True)
