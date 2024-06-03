9025654a714f23b7b477a33011755928c86f2f1f
imamitdev <imamitsinghchauhan@gmail.com>
2024-06-04 00:53:32 +0530

    save data

--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render, HttpResponse
+from .models import Profile
 
 
 # Create your views here.
 def resume(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        mobile_no = request.POST.get("mobile_no")
+        summary = request.POST.get("summary")
+        degree = request.POST.get("degree")
+        school = request.POST.get("school")
+        university = request.POST.get("university")
+        previous = request.POST.get("previous")
+        skills = request.POST.get("skills")
+        resume_profile = Profile(
+            name=name,
+            email=email,
+            mobile_no=mobile_no,
+            summary=summary,
+            degree=degree,
+            school=school,
+            university=university,
+            previous_work=previous,
+            skills=skills,
+        )
+        resume_profile.save()
     return render(request, "resume.html")
